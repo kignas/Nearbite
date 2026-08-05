005ac99a362527e13ca60c5fd47060d2c89f0d01
Sagnik <sagnikroysagnikroy4@gmail.com>
2026-08-05 09:30:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const CONFIG = {
   // Local development URL
   // API_BASE_URL: "http://localhost:5000/api"
   
-  // Production Render URL (Change this to your actual Render app URL before launch!)
-  ​API_BASE_URL: "[https://eatswada.onrender.com/api](https://eatswada.onrender.com/api)"
+  // Production Render URL
+  API_BASE_URL: "https://eatswada.onrender.com/api"
 };
